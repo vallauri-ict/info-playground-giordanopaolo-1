673be58f798925bc9cf1d7cac6bd0586d3120d67
unknown <p.giordano.1320@vallauri.edu>
2021-09-23 16:00:27 +0200

    Homework Done (ES02 in Sql)

---
+++ SQL/02_Sql_Compito/dbo.Aereo.sql
@@ -0,0 +1,5 @@
+﻿create table Aereo
+(	TipoAereo varchar(50) primary key,
+	NumPasseggeri varchar(50) not null,
+	QtaMerci int not null
+)
---
+++ SQL/02_Sql_Compito/dbo.volo.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Aereo] (
+    [Citta]    VARCHAR (50) NOT NULL,
+    [Nazione]  VARCHAR (50) NULL,
+    [NumPiste] INT          NULL,
+    PRIMARY KEY CLUSTERED ([Citta] ASC)
+);
+
