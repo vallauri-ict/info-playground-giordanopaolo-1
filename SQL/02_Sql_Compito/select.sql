6c7b6f754e9069cb730fb94d155da4e123fca546
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2021-09-24 10:50:58 +0200

    Esercise number 2 completed

---
+++ SQL/02_Sql_Compito/select.sql
@@ -0,0 +1,8 @@
+﻿/*1 - Citta in cui si può partire per Roma*/
+SELECT DISTINCT CittaPart from Volo where CittaArr = 'Roma' ORDER BY CittaPart
+
+/*2 - AEREOPORTI IN CUI NON È NOTO IL NUMERO DI PISTE */
+SELECT Citta, Nazione FROM Aereoporto WHERE NumPiste IS NULL ORDER BY Nazione,Citta
+
+/*3 - PER OGNI VOLO MISTO(SIA MERCI CHE PASSEGGERI) DAMMI IL CODICE VOLO E  DATI DI TRASPOSTO*/
+SELECT A.TipoAereo, V.IdVolo FROM Aereo A, Volo V WHERE V.TipoAereo = A.TipoAereo AND A.TipoAereo = 'misto'
---
+++ SQL/02_Sql_Compito/volo.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Volo] (
+    IdVolo    int NOT NULL primary key identity,
+    GiornoSett  VARCHAR (50) NULL,
+    TipoAereo  VARCHAR (50) NULL,
+    AraArr  VARCHAR (50) NULL,
+    CittaArr  VARCHAR (50) NULL,
+	OraPart  VARCHAR (50) NULL,
+    CittaPart INT          NULL
+);
