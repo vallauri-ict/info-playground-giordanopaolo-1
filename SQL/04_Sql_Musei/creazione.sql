624fd5ed19afd6ffb9cd045304124c7ea8aaff4b
unknown <p.giordano.1320@vallauri.edu>
2021-09-30 20:34:46 +0200

    Homework Musei Done 30/09/2021

---
+++ SQL/04_Sql_Musei/creazione.sql
@@ -0,0 +1,21 @@
+﻿Create table Musei(
+	NomeM varchar(50) primary key,
+	Citta varchar(50) not null
+)
+
+Create table Artisti(
+	NomeA varchar(50) primary key,
+	Nazionalita varchar(50) not null
+)
+
+Create table Opere(
+	Codice int primary key,
+	Titolo varchar(50) not null,
+	NomeM varchar(50) FOREIGN KEY ([NomeM]) REFERENCES [Musei],
+	NomeA varchar(50) FOREIGN KEY ([NomeA]) REFERENCES [Artisti]
+)
+
+Create table Personaggi(
+	Personaggio varchar(50) primary key,
+	Codice int FOREIGN KEY ([Codice]) REFERENCES [Opere]
+)
