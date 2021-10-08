70992143bf7008fe2bfb101fe63bb01e93a349cf
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2021-10-08 11:21:37 +0200

    es 05 auto almost done

---
+++ SQL/05_Sql_Auto/crete table.sql
@@ -0,0 +1,35 @@
+﻿Create table Proprietari(
+	CodF int primary key identity(1,1),
+	Nome varchar(50) not null,
+	Residenza varchar(50) not null
+)
+
+Create table Assicurazioni(
+	CodAss int primary key identity(1,1),
+	Nome varchar(50) not null,
+	Sede varchar(50) not null
+)
+
+Create table Auto(
+	Targa varchar(7) primary key,
+	Marca varchar(50) not null,
+	Cilindrata int not null,
+	Potenza int not null,
+	CodF int FOREIGN KEY REFERENCES Proprietari(CodF),
+	CodAss int FOREIGN KEY REFERENCES Assicurazioni(CodAss)
+)
+
+
+
+Create table Sinistro(
+	CodS int primary key identity(1,1),
+	Localita varchar(50) not null,
+	Giorno date not null
+)
+
+Create table Autocoinvolte(
+	CodS int FOREIGN KEY REFERENCES Sinistro(CodS),
+	Targa varchar(7) FOREIGN KEY REFERENCES Auto(Targa),
+	ImportoDelDanno int not null,
+	primary key(CodS,Targa)
+)
---
+++ SQL/05_Sql_Auto/select.sql
@@ -0,0 +1,23 @@
+﻿SELECT targa, marca
+FROM Auto where Cilindrata > 2000 and potenza > 120
+
+SELECT p.Nome, a.Targa
+FROM Auto a, Proprietari p
+where Cilindrata > 2000 and potenza > 120and p.CodF=a.CodF
+
+SELECT a.Targa, p.Nome
+FROM Auto a, Proprietari p, Assicurazioni ass
+WHERE (Cilindrata > 2000 or potenza > 120)and ass.Nome='SARA' and p.CodF=a.CodF and ass.CodAss=a.CodAss
+
+SELECT a.Targa, p.Nome
+FROM auto a, Proprietari p,Sinistro s, Assicurazioni ass, Autocoinvolte au
+WHERE a.CodAss=ass.CodAss and p.CodF=a.CodF and au.CodS=s.CodS and au.Targa=a.Targa and ass.Nome='SARA' and s.Giorno='20/01/2002'
+
+SELECT ass.Sede, ass.Nome, count(*)
+FROM auto a, Assicurazioni ass
+WHERE a.CodAss=ass.CodAss group by ass.Sede,ass.Nome
+
+/*6*/
+SELECT a.Targa, count(*)
+FROM auto a, Autocoinvolte ass, Sinistro s
+WHERE a.Marca='Fiat'and a.Targa=ass.Targa and s.CodS=ass.CodS group by ass.Targa
