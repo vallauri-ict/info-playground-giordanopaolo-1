dfabafe72a1afe4f728b54a30b5ff29a2feea404
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2022-01-29 13:08:55 +0100

    added es 09 on trigger

---
+++ SQL/07_trigger/dbo.Trigger.sql
@@ -0,0 +1,32 @@
+﻿CREATE TRIGGER nuovaAuto
+	ON [dbo].[Table]
+	instead of INSERT
+	AS BEGIN
+		DECLARE @targa varchar(50)
+		DECLARE @cilindrata int
+		DECLARE @prezzo int
+		DECLARE @modello varchar(50)
+		DECLARE @colore varchar(50)
+		DECLARE @marca varchar(50)
+		DECLARE @alimentazione varchar(50)
+		DECLARE @idProp int
+
+		SET @targa = (SELECT targa FROM INSERTED)
+		SET @cilindrata = (SELECT cilindrata FROM INSERTED)
+		SET @prezzo = (SELECT prezzo FROM INSERTED)
+		SET @modello = (SELECT modello FROM INSERTED)
+		SET @colore = (SELECT colore FROM INSERTED)
+		SET @marca = (SELECT marca FROM INSERTED)
+		SET @alimentazione = (SELECT alimentazione FROM INSERTED)
+		SET @idProp = (SELECT idProp FROM INSERTED)
+
+		IF(@cilindrata < 900)
+			SET @cilindrata = 900;
+		IF(@cilindrata > 4500)
+			SET @cilindrata = 4500;
+		
+		INSERT INTO [dbo].[Table]
+			VALUES(@targa, @cilindrata, @prezzo, @modello, @colore, @marca, @alimentazione, @idProp)
+
+
+	END
---
+++ SQL/07_trigger/inserti a caso.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO [dbo].[Table]
+			VALUES('AA111AA', 5, 20000, 'A3', 'Blu', 'AUDI', 'Elettrica', 1)
+			
+INSERT INTO [dbo].[Table]
+			VALUES('BB222BB', 80000, 12000, 'A4', 'Rossa', 'Dacia', 'Legno', 2)
+
+UPDATE [Table]
+			SET prezzo = 20 WHERE targa='BB222BB'
