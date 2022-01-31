0523c9177314f8efda7e15266bc386adc83d264c
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2022-01-31 13:05:59 +0100

    Added a better update

---
+++ SQL/08_trieggerEsercizio/dbo.Trigger.sql
@@ -0,0 +1,10 @@
+﻿CREATE TRIGGER [Trigger]
+	ON [dbo].[Driver]
+	FOR DELETE
+	AS
+	BEGIN
+		SET NOCOUNT ON
+		declare @currDate date
+		set @currDate = CURRENT_TIMESTAMP
+		insert into StoricoCancellazioni select d.number,d.full_name, d.country,d.date_birth, d.team_id, d.podiums_number, @currDate from deleted d
+	END
---
+++ SQL/08_trieggerEsercizio/dbo.Trigger_1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TRIGGER [TriggerUpdate]
+	ON [dbo].[Driver]
+	FOR UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON
+		declare @currDate date
+		set @currDate = CURRENT_TIMESTAMP
+
+		insert into StoricoAggiornamenti(number,full_name,country,date_birth,team_id,podiums_number,numberNew,full_nameNew, countryNew,date_birthNew,team_idNew, podiums_numberNew, [data]) 
+		select d.*, i.*, @currDate from deleted d, inserted i 
+		where d.number=i.number
+	END
