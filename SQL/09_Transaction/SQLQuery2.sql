4f9bbd449b3a67a4ea8caa8217d7417ef5c7cd55
unknown <p.giordano.1320@vallauri.edu>
2022-02-05 12:54:55 +0100

    added es 09

---
+++ SQL/09_Transaction/SQLQuery2.sql
@@ -0,0 +1,21 @@
+﻿BEGIN TRANSACTION tr
+
+	Insert into [Table] values ('Neri',3500.00)
+	SAVE TRANSACTION Punto1
+
+	Insert into [Table] values ('Viola',2300.00)
+	SAVE TRANSACTION Punto2
+
+	Select * from [Table]
+
+	Insert into [Table] values ('Marrone',1000.00)
+	SAVE TRANSACTION Punto3
+
+	Select * from [Table]
+	ROLLBACK TRANSACTION Punto2
+
+	Select * from [Table]
+
+COMMIT;
+
+EXEC Bonifico 100,1,2
---
+++ SQL/09_Transaction/dbo.Procedure.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[Bonifico]
+	@importo float = 0,
+	@contoSorgente int,
+	@contoDestinazione int
+AS
+	if(@contoDestinazione=@contoSorgente)
+		return -2;
+	BEGIN TRANSACTION
+	INSERT INTO Movimenti(Importo,data,Tipo,CdConto)
+		VALUES(@importo,GETDATE(),'P',@contoSorgente)
+	if(@@ERROR<>0)
+		BEGIN
+			ROLLBACK
+				RAISERROR('Errore durante il prelievo',16,1)
+				return -1;
+		END
+	INSERT INTO Movimenti(Importo,data,Tipo,CdConto)
+	VALUES(@importo,GETDATE(),'V',@contoDestinazione)
+
+	if(@@ERROR<>0)
+	BEGIN
+		ROLLBACK
+		RAISERROR('Errore durante il versamento',16,2)
+		return -1;
+	END
+	-- STEP 4: If we reach this point, the commands completed successfully
+	COMMIT
+RETURN 0
